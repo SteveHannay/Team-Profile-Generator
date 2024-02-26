d91ef88689cd61aee5ea00a568dcb72372815c35
Steve Hannay <hannay.steve@googlemail.com>
2024-02-26 16:59:07 +0000

    html design complete

--- src/page-template.js
+++ src/page-template.js
@@ -1,15 +1,24 @@
+/* 
+    Javascript for Challenge 10
+    Developer - Bootcamp with addions made by Steve Hannay (inline Style settings)
+    First Created - 23rd February 2024 (from Bootcamp starter code)
+
+    This code handles HTML Generation for the Team Profile Generator
+*/
+
+
 // creates the team
 const generateTeam = team => {
 
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
+        <div class="card employee-card" style="margin: 20px 20px 20px 20px">
+        <div class="card-header" style="background-color:CornflowerBlue;">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
-        <div class="card-body">
+        <div class="card-body" style="background-color:ghostwhite">
             <ul class="list-group">
                 <li class="list-group-item">ID: ${manager.getId()}</li>
                 <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
@@ -23,12 +32,12 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card" style="margin: 20px 20px 20px 20px">
+    <div class="card-header" style="background-color:LightGreen;">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="background-color:ghostwhite">
         <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
@@ -42,12 +51,12 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card employee-card" style="margin: 20px 20px 20px 20px;">
+    <div class="card-header" style="background-color:powderblue;">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
-    <div class="card-body">
+    <div class="card-body" style="background-color:ghostwhite">
         <ul class="list-group">
             <li class="list-group-item">ID: ${intern.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
@@ -58,17 +67,23 @@ const generateTeam = team => {
         `;
     };
 
+    // build cards
     const html = [];
 
+    // (a) build Manager card from Manager object in Team Array
     html.push(team
         .filter(employee => employee.getRole() === "Manager")
         .map(manager => generateManager(manager))
     );
+
+    // (b) build Engineer card(s) from Engineer object(s) in Team Array
     html.push(team
         .filter(employee => employee.getRole() === "Engineer")
         .map(engineer => generateEngineer(engineer))
         .join("")
     );
+
+    // (c) build Intern card(s) from Intern object(s) in Team Array
     html.push(team
         .filter(employee => employee.getRole() === "Intern")
         .map(intern => generateIntern(intern))
@@ -97,17 +112,17 @@ module.exports = team => {
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
-<body>
+<body style="background-color:PapayaWhip;">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 team-heading" style="padding: 1rem 1rem; background-color:PapayaWhip;">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container" style="background-color:lightgrey">
         <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="team-area col-12 d-flex" style="display: flex; flex-wrap: wrap; flex-direction: row;">
                 ${generateTeam(team)}
             </div>
         </div>
